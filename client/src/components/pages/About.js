0b8f920be86f6c4e462f6cd4241ac888134b2370
Wicherink,Bram B.J <b.wicherink@student.fontys.nl>
2021-05-25 12:53:42 +0200

    Added React and made some components

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Over de Fontys Chatbot</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div><h1>Fontys Chatbot</h1></div>
+    )
+}
+
+export default Landing;
