70cc4c52c3d7d741b9c2e7bdc9926615b7ad117d
Wicherink,Bram B.J <b.wicherink@student.fontys.nl>
2021-05-25 11:17:31 +0200

    Parsed googlePrivateKey variable to JSON

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,10 @@
+const { json } = require("body-parser");
+
 module.exports = {
     // Project ID of DialogFlow app for Fontys PRO
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 }
