c8456aba4478105f919b23d6418b0519485fe70f
Wicherink,Bram B.J <b.wicherink@student.fontys.nl>
2021-07-01 16:08:33 +0200

    Send button now working and designed properly

--- client/README.md
+++ client/README.md
@@ -1,70 +1,19 @@
-# Getting Started with Create React App
+# 100 PROcent de juiste keuze met Chatbot Floris
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Deze chatbot is ontwikkeld als afstudeerproject van Bram Wicherink binnen de afdeling Marketing & Communicatie bij Fontys Hogeschool ICT [Bekijk afstudeerportfolio](afstuderen.bramwicherink.nl).
 
-## Available Scripts
+## Het opstarten van het project
 
-In the project directory, you can run:
+Wanneer het project gecloned is, kan er middels het command
 
-### `npm start`
+### `npm run dev`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+de applicatie op localhost, poort 3000 gerund worden. Ik raad je aan om de applicatie in de live omgeving te bekijken, welke tevens de laatste commit bevat. Deze live omgeving is te [hier](http://salty-eyrie-39216.herokuapp.com) te bekijken. 
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
-### `npm test`
+## Gebruikte technieken
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Er is gebruik gemaakt van een koppeling tussen de back-end en front-end. De basic back-end wordt aangedreven door Express.js die de koppeling met de front-end (React & Node.js) maakt. De front-end zorgt voor de weergave van de data die de backend ontvangt vanuit de API van Dialogflow. De chatbot is namelijk ontwikkeld via Dialogflow (voorheen API.AI) waarmee er middels Natural Language Processing woorden of zinnen herkend kunnen worden waarmee zodoende potentials een juist antwoord op de vraag weten te krijgen. 
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Componenten
+De applicatie is opgebouwd middels verschillende componenten. 
--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -8,28 +8,29 @@ import '../../App.css';
 
 const cookies = new Cookies();
 
-class Chatbot extends Component {   
+class Chatbot extends Component {
 
-    messagesEnd; 
+    messagesEnd;
     // Initialiseert de data component
     constructor(props) {
         super(props);
         // handleInputKeyPress registreert of het invoerveld (voor vragen) al dan niet is geselecteerd of een waarde heeft (ingetypte tekst) 
         this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+        this._handleInputButtonPress = this._handleInputButtonPress.bind(this);
         this.hide = this.hide.bind(this);
-        this.show = this.show.bind(this);   
+        this.show = this.show.bind(this);
         // Definiëren van de state en eigenschappen die later in de code gebruikt kunnen worden
         this.state = {
             messages: [],
             showBot: false,
-        // disableInput kijkt naar of het invoerveld al dan niet uitgeschakeld dient te worden
+            // disableInput kijkt naar of het invoerveld al dan niet uitgeschakeld dient te worden
             disableInput: false,
-        // typingIndicator kijkt naar of er vanuit de API responses komen 
+            // typingIndicator kijkt naar of er vanuit de API responses komen 
             typingIndicator: false
         }
         // Zorgt voor een unieke userID waarmee iedere keer dat de pagina herladen wordt een andere sessie gestart wordt. Dit is benodigd voor Dialogflow zodat de verschillende sessies o.a. opgeslagen kunnen worden ter analyse van iedere dialoog.
-        if(cookies.get('userID') === undefined) {
-        cookies.set('userID', uuid(), {path: '/'});
+        if (cookies.get('userID') === undefined) {
+            cookies.set('userID', uuid(), { path: '/' });
         }
     }
 
@@ -51,24 +52,24 @@ class Chatbot extends Component {
         // Opvragen van gegevens in de backend / API vanuit Dialogflow
         try {
             const res = await axios.post('api/df_text_query', { text: queryText, userID: cookies.get('userID') });
-            for(let msg of res.data.fulfillmentMessages) {
-            
+            for (let msg of res.data.fulfillmentMessages) {
+
                 says = {
                     speaks: 'bot',
                     msg: msg
                 }
                 // Kleine vertraging om de conversatie zo natuurlijk mogelijk te laten aanvoelen -> vanuit chatbot
-                await new Promise(resolve => setTimeout(resolve, 1100)); 
-                this.setState({ typingIndicator: true })  
-                this.setState({ messages: [...this.state.messages, says]});
+                await new Promise(resolve => setTimeout(resolve, 1100));
+                this.setState({ typingIndicator: true })
+                this.setState({ messages: [...this.state.messages, says] });
             }
-            this.setState({disableInput: !this.state.disableInput});
+            this.setState({ disableInput: !this.state.disableInput });
             this.setState({ typingIndicator: false });
 
         }
 
         // Wanneer de chatbotwidget geen verbinding kan maken met de Dialogflow API services, geeft deze een foutmelding middels catch statement weer. 
-        catch(e) {
+        catch (e) {
             says = {
                 speaks: 'bot',
                 msg: {
@@ -78,30 +79,30 @@ class Chatbot extends Component {
                 }
             }
             // Wanneer chatbotwidget geen verbinding krijgt, zal de chatbot na 2000ms = 2 seconden dichtklappen
-            this.setState({ messages: [...this.state.messages, says]});
+            this.setState({ messages: [...this.state.messages, says] });
             let that = this;
-            setTimeout(function() {
-                that.setState({ showBot: false});
+            setTimeout(function () {
+                that.setState({ showBot: false });
             }, 2000);
-            
+
         }
 
         // Checkt voor iedere 'message' of het vanuit de 'bot' of 'user' komt. 
-        
+
     };
 
-    
+
     // Checkt voor bepaalde events vanuit Dialogflow 
     async df_event_query(event) {
         const res = await axios.post('api/df_event_query', { event, userID: cookies.get('userID') })
 
-        for(let msg of res.data.fulfillmentMessages) {
+        for (let msg of res.data.fulfillmentMessages) {
 
             let says = {
                 speaks: 'bot',
                 msg: msg
             }
-            this.setState({ messages: [...this.state.messages, says]});
+            this.setState({ messages: [...this.state.messages, says] });
         }
     }
     // Wanneer component/pagina volledig gerenderd is, zal het event 'Welcome' vanuit Dialogflow gestart worden. Dit zorgt voor het weergeven van het welkomstbericht.
@@ -116,36 +117,36 @@ class Chatbot extends Component {
     show(event) {
         event.preventDefault();
         event.stopPropagation();
-        this.setState({showBot: true});
+        this.setState({ showBot: true });
     }
-     // Wanneer de chatbotknop state 'open' geklikt wordt, wordt er een show methode aangeroepen die ervoor zorgt dat de chatbotwidget daadwerkelijk gesloten wordt
+    // Wanneer de chatbotknop state 'open' geklikt wordt, wordt er een show methode aangeroepen die ervoor zorgt dat de chatbotwidget daadwerkelijk gesloten wordt
     hide(event) {
         event.preventDefault();
         event.stopPropagation();
-        this.setState({showBot: false});
+        this.setState({ showBot: false });
     }
 
-        // Zorgt voor het renderen van de cards wanneer deze aangeroepen worden vanuit de API
+    // Zorgt voor het renderen van de cards wanneer deze aangeroepen worden vanuit de API
     renderCards(cards) {
-        return cards.map((card, i) => <Card key={i} payload={card.structValue}/>);
-        }
+        return cards.map((card, i) => <Card key={i} payload={card.structValue} />);
+    }
 
     // Zorgt voor het opmaken van de card met correcte stijl 
 
     renderOneMessage(message, i) {
-        if(message.msg && message.msg.text && message.msg.text.text) {
+        if (message.msg && message.msg.text && message.msg.text.text) {
             return <Message key={i} speaks={message.speaks} text={message.msg.text.text} />;
         }
-        else if(message.msg && message.msg.payload && message.msg.payload.fields && message.msg.payload.fields.cards) {
-            return  <div key={i} style={{overflow: 'scroll hidden'}}>
+        else if (message.msg && message.msg.payload && message.msg.payload.fields && message.msg.payload.fields.cards) {
+            return <div key={i} style={{ overflow: 'scroll hidden' }}>
                 <div className="card card-fontys">
-                    <div style={{overflow: 'scroll hidden'}}>
+                    <div style={{ overflow: 'scroll hidden' }}>
                     </div>
-                 <div style={{overflow: 'scroll hidden', marginBottom: '30px'}}>
-                     <div style={{height: '100%', width: message.msg.payload.fields.cards.listValue.values.length * 270, display: 'flex'}}>
-                        {this.renderCards(message.msg.payload.fields.cards.listValue.values)}
-                     </div>
-                 </div>
+                    <div style={{ overflow: 'scroll hidden', marginBottom: '30px' }}>
+                        <div style={{ height: '100%', width: message.msg.payload.fields.cards.listValue.values.length * 270, display: 'flex', overflow: 'auto scroll'}}>
+                            {this.renderCards(message.msg.payload.fields.cards.listValue.values)}
+                        </div>
+                    </div>
                 </div>
             </div>
         }
@@ -153,7 +154,7 @@ class Chatbot extends Component {
     }
     // Methode die renderCards & renderOneMessage samen pakt en zorgt voor de weergave
     renderMessages(stateMessages) {
-        if(stateMessages) {
+        if (stateMessages) {
             return stateMessages.map((message, i) => {
                 return this.renderOneMessage(message, i);
             });
@@ -165,119 +166,143 @@ class Chatbot extends Component {
     // Zorgt voor het disablen en enablen van het invoerveld, zolang er berichten vanuit de chatbot zelf worden verzonden, om een overload aan berichten van de potential/user/gebruiker te voorkomen
 
     _handleInputKeyPress(e) {
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' || e.onClick) {
             this.df_text_query(e.target.value);
             e.target.value = '';
-            this.setState({disableInput: !this.state.disableInput});
+            this.setState({ disableInput: !this.state.disableInput });
 
         }
     }
+
+    _handleInputButtonPress(e) {
+        console.log('HandleInputButtonPress')
+        var inputFieldQuestion = document.getElementById('chatbotInput').value;
+        console.log("Test");
+        this.df_text_query(inputFieldQuestion);
+        document.getElementById('chatbotInput').value = '';
+        this.setState({ disableInput: !this.state.disableInput });
+    }
+
+
 
     // Zorgt voor het daadwerkelijk renderen van de elementen bij elkaar
 
     render() {
         // Wanneer de state showBot 'true' is, dus wanneer de chatbotwidget geopend is
-        if(this.state.showBot) {
+        if (this.state.showBot) {
             // Wanneer de chatbot zelf aan het typen is en de typingIndicator (true) verschijnt
-            if(this.state.typingIndicator) {
+            if (this.state.typingIndicator) {
                 // Laat dit weergeven: 
                 return (
                     <div>
                         <button id="toggleChatbot-open" onClick={this.hide}><img alt="X" src={"https://chatbot-fhict.bramwicherink.nl/images/letter-x.svg"}></img></button>
-                       
-                      <div className="chatbotWidget-wrapper" style={{position: 'fixed', display: 'flex', right: '10vw', float: 'right', height: '100%', width: 'auto', top: '7%'}}>
-                          <div className="chatbotWidget">
-                              <div className="chatbotWidget-header">
-                              <h2>Chatbot Floris</h2>
-                              <p>Ik help je graag als je vragen hebt!</p>
-                              </div>
-                              <div className="messageBox">
-                                
-                              {this.renderMessages(this.state.messages)}
-                              <div className="message-typing-indicator-container" ref={(el) => { this.messagesEnd = el; }} style={{float: 'left', clear: 'both'}}>
-                              <div className="typing-indicator">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
+
+                        <div className="chatbotWidget-wrapper" style={{ position: 'fixed', display: 'flex', right: '10vw', float: 'right', height: '100%', width: 'auto', top: '7%' }}>
+                            <div className="chatbotWidget">
+                                <div className="chatbotWidget-header">
+                                    <h2>Chatbot Floris</h2>
+                                    <p>Ik help je graag als je vragen hebt!</p>
                                 </div>
-                                
-                              </div>
+                                <div className="messageBox">
 
-                              
-            
-                              </div>
-                              
-                              <input type="text" 
-            
-                              style={{height: '10%', width: '95%', 
-                              backgroundColor: '#FFFFFF', boxShadow: '0px 4px 24px rgba(0, 0,0, 0.08', 
-                              border: 'none', color: '#484848', paddingLeft: '5%',
-                              fontSize: '16px', right: '0', bottom: '0',
-                              float: 'bottom', position: 'sticky'}} 
-                              
-                              placeholder="Typ je vraag hier..." 
-                              disabled={this.state.disableInput}
-                              onKeyPress={this._handleInputKeyPress}></input>
-                            
-                              
-                            
-                          </div>
-                      </div>  
+                                    {this.renderMessages(this.state.messages)}
+                                    <div className="message-typing-indicator-container" ref={(el) => { this.messagesEnd = el; }} style={{ float: 'left', clear: 'both' }}>
+                                        <div className="typing-indicator">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                        </div>
+
+                                    </div>
+
+
+
+                                </div>
+
+                                <div className="inputField">
+
+                                <input type="text" id="chatbotInput"
+
+                                    style={{
+                                        height: '100%', width: '95%',
+                                        backgroundColor: '#FFFFFF', boxShadow: '0px 4px 24px rgba(0, 0,0, 0.08',
+                                        border: 'none', color: '#484848', paddingLeft: '5%',
+                                        fontSize: '16px', right: '0', bottom: '0',
+                                        float: 'bottom', position: 'sticky'
+                                    }}
+
+                                    placeholder="Typ je vraag hier..."
+                                    disabled={this.state.disableInput}
+                                    onKeyPress={this._handleInputKeyPress}></input>
+
+<input className="arrowIcon" type="image" style={{ float: 'bottom', position: 'sticky' }} src="https://chatbot-fhict.bramwicherink.nl/images/arrow.svg" onClick={this._handleInputButtonPress} disabled={this.state.disableInput}></input>
+
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
-                    )
+                )
             }
-        return (
-        <div>
-            <button id="toggleChatbot-open" onClick={this.hide}><img alt="X" src={"https://chatbot-fhict.bramwicherink.nl/images/letter-x.svg"}></img></button>
-           
-          <div className="chatbotWidget-wrapper" style={{position: 'fixed', display: 'flex', right: '10vw', float: 'right', height: '100%', width: 'auto', top: '7%'}}>
-              <div className="chatbotWidget">
-                  <div className="chatbotWidget-header">
-                  <h2>Chatbot Floris</h2>
-                  <p>Ik help je graag als je vragen hebt!</p>
-                  </div>
-                  <div className="messageBox">
-                  {this.renderMessages(this.state.messages)}
-                  <div ref={(el) => { this.messagesEnd = el; }} style={{float: 'left', clear: 'both'}}>
+            return (
+                <div>
+                    <button id="toggleChatbot-open" onClick={this.hide}><img alt="X" src={"https://chatbot-fhict.bramwicherink.nl/images/letter-x.svg"}></img></button>
 
-                  </div>
+                    <div className="chatbotWidget-wrapper" style={{ position: 'fixed', display: 'flex', right: '10vw', float: 'right', height: '100%', width: 'auto', top: '7%' }}>
+                        <div className="chatbotWidget">
+                            <div className="chatbotWidget-header">
+                                <h2>Chatbot Floris</h2>
+                                <p>Ik help je graag als je vragen hebt!</p>
+                            </div>
+                            <div className="messageBox">
+                                {this.renderMessages(this.state.messages)}
+                                <div ref={(el) => { this.messagesEnd = el; }} style={{ float: 'left', clear: 'both' }}>
 
-                  </div>
-                  
-                  <input type="text" 
-            
-                              style={{height: '10%', width: '95%', 
-                              backgroundColor: '#FFFFFF', boxShadow: '0px 4px 24px rgba(0, 0,0, 0.08', 
-                              border: 'none', color: '#484848', paddingLeft: '5%',
-                              fontSize: '16px', right: '0', bottom: '0',
-                              float: 'bottom', position: 'sticky'}} 
-                              
-                              placeholder="Typ je vraag hier..." 
-                              disabled={this.state.disableInput}
-                              onKeyPress={this._handleInputKeyPress}></input>
-                  
-                  
-                  
-                  
+                                </div>
+
+                            </div>
+
+                            <div className="inputField">
+
+                                <input type="text" id="chatbotInput"
+
+                                    style={{
+                                        height: '100%', width: '95%',
+                                        backgroundColor: '#FFFFFF', boxShadow: '0px 4px 24px rgba(0, 0,0, 0.08',
+                                        border: 'none', color: '#484848', paddingLeft: '5%',
+                                        fontSize: '16px', right: '0', bottom: '0',
+                                        float: 'bottom', position: 'sticky', cursor: 'pointer'
+                                    }}
+
+                                    placeholder="Typ je vraag hier..."
+                                    disabled={this.state.disableInput}
+                                    onKeyPress={this._handleInputKeyPress}></input>
 
 
-                  
-              </div>
-          </div>  
-        </div>
-        )
-      }
-      else {
-        return (
-            <div>
-            <button id="toggleChatbot-open" onClick={this.show}><img alt="Open" src={"https://chatbot-fhict.bramwicherink.nl/images/floris-icon.svg"}></img></button>
-            <div ref={(el) => { this.messagesEnd = el; }} style={{float: 'left', clear: 'both'}}>
 
-                  </div>
-            </div>
-            
-          )
-      }
+                                <input className="arrowIcon" type="image" style={{ float: 'bottom' }} src="https://chatbot-fhict.bramwicherink.nl/images/arrow.svg" onClick={this._handleInputButtonPress} disabled={this.state.disableInput}></input>
+
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <button id="toggleChatbot-open" onClick={this.show}><img alt="Open" src={"https://chatbot-fhict.bramwicherink.nl/images/floris-icon.svg"}></img></button>
+                    <div ref={(el) => { this.messagesEnd = el; }} style={{ float: 'left', clear: 'both' }}>
+
+                    </div>
+                </div>
+
+            )
+        }
     }
 }
 
